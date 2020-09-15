2d33ce36ff528df92c997aad68ad32a2811905c2
Yuka Watanabe <yuuka.wtnb.011@gmail.com>
2020-09-15 22:56:31 +0900

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Yuuka Watanabe on 2020/09/15.
+//  Copyright © 2020 Yuka Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
